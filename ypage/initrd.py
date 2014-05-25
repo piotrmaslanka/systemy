82845b4caf8046aa7ed2fb27ca5feeaf55f12e79
henrietta <piotr.maslanka@henrietta.com.pl>
2014-05-25 15:04:39 +0200

    init tasklet added

---
+++ ypage/initrd.py
@@ -0,0 +1,18 @@
+"""
+System y init list
+
+
+Here you can specify tasklets you want to load on startup, in the form of tuples
+
+    first element of tuple is a str, with target user value
+    second element of tuple is a str, with target group value
+    third element of tuple is a str, with target name value
+    fourth element of tuple is either a str with path to tasklet class, or tasklet class itself
+        ( so last element is the class name )
+    
+    fifth OPTIONAL element of tuple is an args tuple
+    sixth OPTIONAL element of a tuple is a kwargs dictionary
+"""
+initrd = (       
+    ('USER', 'group', 'helloWorld', 'examples.HelloWorld.HelloWorldTasklet'),
+)
--- ypage/run.py
+++ ypage/run.py
@@ -2,8 +2,36 @@ from ypage.nukleon import S
 
 import ypage.apipatchers    # this fixes yos module imports
 
-from examples.tcpConnExample import NetworkTest
 from ypage.nukleon.structs import TaskletControlBlock
+from ypage.initrd import initrd
+from yos.rt import GCTasklet
+from yos.tasklets import Tasklet
+
+import importlib
+
+class InitTasklet(GCTasklet):
+    def __init__(self, initlist):
+        self.initlist = initlist
+        
+    def on_startup(self):
+        print ("INIT: Init v1.0 (part of System y) started")
+        for elem in self.initlist:
+            if len(elem) == 4: user, group, name, cls = elem; args = (); kwargs = {}
+            if len(elem) == 5: user, group, name, cls, args = elem; kwargs = {}
+            if len(elem) == 6: user, group, name, cls, args, kwargs = elem
+            
+            if isinstance(cls, str):    
+                # attempt an import
+                try:
+                    mod = importlib.import_module('.'.join(cls.split('.')[:-1]))
+                    cls = getattr(mod, cls.split('.')[-1])
+                except ImportError:
+                    print("INIT: Failed to import", cls)
+                    continue
+                
+            Tasklet.start(cls, name, group, user, None, *args, **kwargs) 
+
+            print("INIT:", len(self.initlist), "tasklets spawned, exiting")
 
 if __name__ == '__main__':
     S.startup()
@@ -11,7 +39,7 @@ if __name__ == '__main__':
 
     
     # manual bootstrap here
-    hwt = NetworkTest()
-    tcb = TaskletControlBlock(0, 'SYSTEMYA', 'admin', 'support')
-    S.registerNewTasklet(tcb, hwt)
-    S.schedule(tcb, hwt.on_startup)
+    init = InitTasklet(initrd)
+    tcb = TaskletControlBlock(0, 'SYSTEMYA', 'admin', 'init')
+    S.registerNewTasklet(tcb, init)
+    S.schedule(tcb, init.on_startup)
