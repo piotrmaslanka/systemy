358d402710479be9ade2d2e84bf02ae4a0bcc72c
henrietta <piotr.maslanka@henrietta.com.pl>
2014-05-27 01:50:30 +0200

    Catalog scatter-gather added
    closes #11

--- yos/ipc/Catalog.py
+++ yos/ipc/Catalog.py
@@ -32,3 +32,16 @@ class Catalog(object):
         
         Alternatively, an exception class can be returned if entry doesn't exist
         """
+        
+    @staticmethod
+    def gather(keys, result1: callable, catname: str=''):
+        """
+        Queries multiple keys. When results are available, calls result1
+        with a dictionary that maps keys to values recovered. A Catalog.NotFoundError
+        class will be the value for given key if no entry was found
+        """
+        
+    @staticmethod
+    def scatter(kv: dict, catname: str=''):
+        """Updates multiple keys at once in given catalog.
+        kv contains keys and new values"""
--- ypage/apipatchers/ipcCatalog.py
+++ ypage/apipatchers/ipcCatalog.py
@@ -30,5 +30,21 @@ class CatalogHandler(Catalog):
             else:
                 S.schedule(S.loc.tcb, result1, val)
         
+    @staticmethod
+    def gather(keys, result1: callable, catname: str=''):
+        result = {}
+        with CatalogHandler.catalog_lock:
+            for key in keys:
+                try:
+                    result[key] = CatalogHandler.catalog_root[catname][key]
+                except KeyError:
+                   result[key] = Catalog.NotFoundError
+        S.schedule(S.loc.tcb, result1, result)
+                
+    @staticmethod
+    def scatter(kv: dict, catname: str=''):
+        with CatalogHandler.catalog_lock:
+            CatalogHandler.catalog_root[catname].update(kv)
+        
 import yos.ipc
 yos.ipc.Catalog = CatalogHandler        
