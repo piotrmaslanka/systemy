d261cc2520c81078468a5faa5ebe6142cb25a56c
henrietta <piotr.maslanka@henrietta.com.pl>
2014-05-30 02:00:30 +0200

    allow n-ary default callback in NCounter

--- yos/rt/ncounter.py
+++ yos/rt/ncounter.py
@@ -19,7 +19,7 @@ class NCounter(object):
         self.times = times
         self.callback_ = callback
         
-    def __call__(self, clbck: callable=lambda: None):
+    def __call__(self, clbck: callable=lambda *args, **kwargs: None):
         
         def cb(*args, **kwargs):
             self.times -= 1
